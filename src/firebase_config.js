734cee7d859c3092f9f585d510d7281959037133
AkashPatel18 <48157243+AkashPatel18@users.noreply.github.com>
2021-04-19 21:29:20 +0530

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDgHRPn65HI8t0ylnBITqvPxjA2EXrXR2M",
-    authDomain: "todoapp-312ef.firebaseapp.com",
-    projectId: "todoapp-312ef",
-    storageBucket: "todoapp-312ef.appspot.com",
-    messagingSenderId: "1017786580328",
-    appId: "1:1017786580328:web:80cc29bd2930d211e02add"
+    ///add key
 };
 
 firebase.initializeApp(firebaseConfig);
